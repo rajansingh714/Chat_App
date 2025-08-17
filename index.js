4c8ad8604265cb04323192c6088e5560d0dc9351
Your Name <rajansingh70707@gmail.com>
2025-08-17 16:13:54 +0530

    added model

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
 
+const connect = require("./config/database-config");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use("/", express.static(__dirname + "/public"));
-
 io.on("connection", (socket) => {
-  console.log("a user connected");
-
   socket.on("msg_send", (data) => {
     // console.log("event coming from client", data);
     // io.emit("msg_rcvd", data);
@@ -19,6 +17,18 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+app.set("view engine", "ejs");
+app.use("/", express.static(__dirname + "/public"));
+
+app.get("/chat/:roomid", (req, res) => {
+  res.render("index", {
+    name: "Rajan",
+    id: req.params.roomid,
+  });
+});
+
+server.listen(3000, async () => {
   console.log(`server is started`);
+  await connect();
+  console.log("mongodb connected");
 });
